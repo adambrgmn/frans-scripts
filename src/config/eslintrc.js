427f2f6a4051c9e6e80737eab9237ce598adf08e
Adam Bergman <adam@fransvilhelm.com>
2017-11-29 14:36:26 +0100

    fix(eslint): Use babel-eslint parser
    The eslint config will now use babel-eslint as its parser. So that things like class-properties work
    out of the box. This commit also adds a couple other things such as env and rules
    
    fix #3

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,10 +1,30 @@
 const { ifAnyDep } = require('../utils');
 
 module.exports = {
+  parser: 'babel-eslint',
   extends: [
     require.resolve('eslint-config-airbnb-base'),
     ifAnyDep('react', require.resolve('eslint-config-airbnb')),
     require.resolve('eslint-config-prettier'),
   ].filter(Boolean),
-  rules: {},
+  plugins: ['babel'],
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    'shared-node-browser': true,
+    es6: true,
+    worker: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: { experimentalObjectRestSpread: true },
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/sort-comp': 'off',
+  },
 };
